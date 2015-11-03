07aa812f8f13da37d3c44ea17a81f0a5d8184309
lionmedia <tracey@lionmedia.com>
2015-11-03 09:27:36 -0500

    modified:   css/main.css 	modified:   js/app.js 	modified:   main.html

--- js/app.js
+++ js/app.js
@@ -8,16 +8,6 @@ $(document).ready(function(){
         $('.ryu-ready').hide();
     })
     .mousedown(function(){
-        $(document).ready(function(){
-    $('.ryu').mouseenter(function(){
-        $('.ryu-still').hide();
-        $('.ryu-ready').show();
-    })
-    .mouseleave(function() {
-        $('.ryu-still').show();
-        $('.ryu-ready').hide();
-    })
-    .mousedown(function(){
         playHadouken();
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
@@ -32,28 +22,22 @@ $(document).ready(function(){
     .mouseup(function(){
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
-    });
+    //});
+      });
 
-});
-
-function playHadouken() {
-    $('#hadouken-sound')[0].volume = 0.5;
-    $('#hadouken-sound')[0].load();
-    $('#hadouken-sound')[0].play();
-}
+    $("body").keydown(function(e){
+        if (e.which == '88') {
+        $('.ryu-still').hide();
         $('.ryu-ready').hide();
-        $('.ryu-throwing').show();
-        $('.hadouken').finish().show().animate(
-            {'left': '1020px'},
-            500,
-            function(){
-                $(this).hide();
-                $(this).css('left', '520px');
-            });
-    })
-    .mouseup(function(){
         $('.ryu-throwing').hide();
-        $('.ryu-ready').show();
+        $('.ryu-cool').show();
+
+    }
+     })
+     .keyup(function() {
+        $('.ryu-still').show();
+        $('.ryu-cool').hide();
+     
     });
 
 });
